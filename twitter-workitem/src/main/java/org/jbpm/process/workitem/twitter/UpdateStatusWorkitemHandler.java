1ba2f3fbed67a1587deab095ffa42d5ce3052358
Tihomir Surdilovic <tsurdilo@redhat.com>
2018-01-10 05:54:38 -0800

    Twitter workitem (#20)

---
+++ twitter-workitem/src/main/java/org/jbpm/process/workitem/twitter/UpdateStatusWorkitemHandler.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jbpm.process.workitem.twitter;
+
+import java.io.ByteArrayInputStream;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import org.jbpm.document.Document;
+import org.jbpm.process.workitem.core.AbstractLogOrThrowWorkItemHandler;
+import org.jbpm.process.workitem.core.util.Wid;
+import org.jbpm.process.workitem.core.util.WidMavenDepends;
+import org.jbpm.process.workitem.core.util.WidParameter;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+
+@Wid(widfile = "TwitterUpdateStatus.wid", name = "TwitterUpdateStatus",
+        displayName = "TwitterUpdateStatus",
+        defaultHandler = "mvel: new org.jbpm.process.workitem.twitter.UpdateStatusWorkitemHandler()",
+        parameters = {
+                @WidParameter(name = "StatusUpdate"),
+                @WidParameter(name = "Media"),
+                @WidParameter(name = "DebugEnabled")
+        },
+        mavenDepends = {
+                @WidMavenDepends(group = "org.twitter4j", artifact = "twitter4j-core", version = "4.0.6")
+        })
+public class UpdateStatusWorkitemHandler extends AbstractLogOrThrowWorkItemHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(UpdateStatusWorkitemHandler.class);
+
+    private TwitterAuth auth = new TwitterAuth();
+
+    private String consumerKey;
+    private String consumerSecret;
+    private String accessKey;
+    private String accessSecret;
+    private StatusUpdate statusUpdate;
+
+    public UpdateStatusWorkitemHandler(String consumerKey,
+                                       String consumerSecret,
+                                       String accessKey,
+                                       String accessSecret) {
+        this.consumerKey = consumerKey;
+        this.consumerSecret = consumerSecret;
+        this.accessKey = accessKey;
+        this.accessSecret = accessSecret;
+    }
+
+    public void executeWorkItem(WorkItem workItem,
+                                WorkItemManager workItemManager) {
+
+        String statusMessage = (String) workItem.getParameter("StatusUpdate");
+
+        // media is optional
+        Document statusMedia = null;
+        if (workItem.getParameter("Media") != null) {
+            statusMedia = (Document) workItem.getParameter("Media");
+        }
+
+        // debug is optional (default to false)
+        boolean debugOption = false;
+        if (workItem.getParameter("DebugEnabled") != null) {
+            debugOption = Boolean.parseBoolean((String) workItem.getParameter("DebugEnabled"));
+        }
+
+        if (StringUtils.isNotEmpty(statusMessage)) {
+
+            try {
+                Twitter twitter = auth.getTwitterService(this.consumerKey,
+                                                         this.consumerSecret,
+                                                         this.accessKey,
+                                                         this.accessSecret,
+                                                         debugOption);
+
+                statusUpdate = new StatusUpdate(statusMessage);
+                if (statusMedia != null) {
+
+                    statusUpdate.setMedia(FilenameUtils.getBaseName(statusMedia.getName()) +
+                                                  "." + FilenameUtils.getExtension(statusMedia.getName()),
+                                          new ByteArrayInputStream(statusMedia.getContent()));
+                }
+
+                twitter.updateStatus(statusUpdate);
+
+                workItemManager.completeWorkItem(workItem.getId(),
+                                                 null);
+            } catch (Exception e) {
+                handleException(e);
+            }
+        } else {
+            logger.error("Missing status message.");
+            throw new IllegalArgumentException("Missing status message.");
+        }
+    }
+
+    public void abortWorkItem(WorkItem wi,
+                              WorkItemManager wim) {
+    }
+
+    // for testing
+    public void setAuth(TwitterAuth auth) {
+        this.auth = auth;
+    }
+
+    // for testing
+    public StatusUpdate getStatusUpdate() {
+        return this.statusUpdate;
+    }
+}
---
+++ twitter-workitem/src/test/java/org/jbpm/process/workitem/twitter/TwitterWorkitemHandlerTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jbpm.process.workitem.twitter;
+
+import org.drools.core.process.instance.impl.WorkItemImpl;
+
+import org.jbpm.document.service.impl.DocumentImpl;
+import org.jbpm.process.workitem.core.TestWorkItemManager;
+import org.jbpm.test.AbstractBaseTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mock;
+
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import twitter4j.DirectMessage;
+import twitter4j.Status;
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+
+import static org.junit.Assert.*;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+public class TwitterWorkitemHandlerTest extends AbstractBaseTest {
+
+    @Mock
+    Twitter twitter;
+
+    @Mock
+    TwitterAuth auth;
+
+    @Mock
+    Status status;
+
+    @Mock
+    DirectMessage directMessage;
+
+    @Before
+    public void setUp() {
+        try {
+            when(auth.getTwitterService(anyString(),
+                                        anyString(),
+                                        anyString(),
+                                        anyString(),
+                                        any(boolean.class))).thenReturn(twitter);
+            when(twitter.updateStatus(any(StatusUpdate.class))).thenReturn(status);
+            when(twitter.sendDirectMessage(anyString(),
+                                           anyString())).thenReturn(directMessage);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testUpdateStatus() throws Exception {
+        TestWorkItemManager manager = new TestWorkItemManager();
+        WorkItemImpl workItem = new WorkItemImpl();
+        workItem.setParameter("StatusUpdate",
+                              "testUpdateStatus");
+
+        UpdateStatusWorkitemHandler handler = new UpdateStatusWorkitemHandler("testConsumerKey",
+                                                                              "testConsumerSecret",
+                                                                              "testAccessKey",
+                                                                              "testAccessSecret");
+        handler.setAuth(auth);
+
+        handler.executeWorkItem(workItem,
+                                manager);
+        assertNotNull(manager.getResults());
+        assertEquals(1,
+                     manager.getResults().size());
+        assertTrue(manager.getResults().containsKey(workItem.getId()));
+
+        StatusUpdate handlerStatusUpdate = handler.getStatusUpdate();
+        assertNotNull(handlerStatusUpdate);
+        assertEquals("testUpdateStatus",
+                     handlerStatusUpdate.getStatus());
+    }
+
+    @Test
+    public void testUpdateStatusWithMedia() throws Exception {
+        DocumentImpl testMediaDoc = new DocumentImpl();
+        testMediaDoc.setContent(new String("Test media to send").getBytes());
+        testMediaDoc.setName("testMediaToSend.txt");
+
+        TestWorkItemManager manager = new TestWorkItemManager();
+        WorkItemImpl workItem = new WorkItemImpl();
+        workItem.setParameter("StatusUpdate",
+                              "testUpdateStatus");
+        workItem.setParameter("Media",
+                              testMediaDoc);
+
+        UpdateStatusWorkitemHandler handler = new UpdateStatusWorkitemHandler("testConsumerKey",
+                                                                              "testConsumerSecret",
+                                                                              "testAccessKey",
+                                                                              "testAccessSecret");
+        handler.setAuth(auth);
+
+        handler.executeWorkItem(workItem,
+                                manager);
+        assertNotNull(manager.getResults());
+        assertEquals(1,
+                     manager.getResults().size());
+        assertTrue(manager.getResults().containsKey(workItem.getId()));
+
+        StatusUpdate handlerStatusUpdate = handler.getStatusUpdate();
+        assertNotNull(handlerStatusUpdate);
+        assertEquals("testUpdateStatus",
+                     handlerStatusUpdate.getStatus());
+    }
+
+    @Test
+    public void testSendDirectMessage() throws Exception {
+        TestWorkItemManager manager = new TestWorkItemManager();
+        WorkItemImpl workItem = new WorkItemImpl();
+        workItem.setParameter("Message",
+                              "hello there");
+        workItem.setParameter("ScreenName",
+                              "testScreenName");
+
+        SendDirectMessageWorkitemHandler handler = new SendDirectMessageWorkitemHandler("testConsumerKey",
+                                                                                        "testConsumerSecret",
+                                                                                        "testAccessKey",
+                                                                                        "testAccessSecret");
+        handler.setAuth(auth);
+
+        handler.executeWorkItem(workItem,
+                                manager);
+        assertNotNull(manager.getResults());
+        assertEquals(1,
+                     manager.getResults().size());
+        assertTrue(manager.getResults().containsKey(workItem.getId()));
+    }
+}
