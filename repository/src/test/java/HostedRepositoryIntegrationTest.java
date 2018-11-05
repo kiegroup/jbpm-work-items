ae07417c783dd5b0796992cce44dafb3448b0dc2
tsurdilo <tsurdilo@redhat.com>
2018-11-05 13:51:19 -0800

    pastebin workitem

--- repository/src/test/java/HostedRepositoryIntegrationTest.java
+++ repository/src/test/java/HostedRepositoryIntegrationTest.java
@@ -100,7 +100,7 @@ public class HostedRepositoryIntegrationTest {
         Map<String, WorkDefinitionImpl> repoResults = WorkItemRepository.getWorkDefinitions(uri.toString());
         assertNotNull(repoResults);
         // update this when new workitems are added
-        assertEquals(62,
+        assertEquals(64,
                      repoResults.size());
     }
 
