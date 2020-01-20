2b24fa241d36db3af957b90c1741f4408f3bc410
Marián Macik <mmacik@redhat.com>
2020-01-20 09:34:32 +0100

    DROOLS-4946 - Karaf integration tests fail (#120)

--- karaf-itests/src/test/java/org/jbpm/process/workitem/camel/karaf/itests/AbstractKarafIntegrationTest.java
+++ karaf-itests/src/test/java/org/jbpm/process/workitem/camel/karaf/itests/AbstractKarafIntegrationTest.java
@@ -182,7 +182,7 @@ public class AbstractKarafIntegrationTest {
         }
         options.add(editConfigurationFilePut("etc/org.ops4j.pax.url.mvn.cfg",
                                              "org.ops4j.pax.url.mvn.repositories",
-                                             "http://repo1.maven.org/maven2@id=central," +
+                                             "https://repo1.maven.org/maven2@id=central," +
                                                      "https://repository.jboss.org/nexus/content/groups/public@snapshots@id=jboss-public" +
                                                      additionalMavenRepositories
         ));
