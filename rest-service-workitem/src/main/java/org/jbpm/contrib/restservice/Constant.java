f14d586d7344e2f30cebdba57bdfe70ea1948441
Matej Lazar <matejonnet@gmail.com>
2020-10-14 23:03:19 +0200

    Add cancel url template.

--- rest-service-workitem/src/main/java/org/jbpm/contrib/restservice/Constant.java
+++ rest-service-workitem/src/main/java/org/jbpm/contrib/restservice/Constant.java
@@ -12,4 +12,6 @@ public class Constant {
     public static final String KIE_HOST_SYSTEM_PROPERTY = "KIE_HOSTNAME";
 
     public static final String CANCEL_URL_JSON_POINTER_VARIABLE = "cancelUrlJsonPointer";
+
+    public static final String CANCEL_URL_TEMPLATE_VARIABLE = "cancelUrlTemplate";
 }
