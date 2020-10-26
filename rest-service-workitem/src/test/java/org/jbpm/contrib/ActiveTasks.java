eed81e8273642b96bfff98eb5fff7b7da80557c4
Matej Lazar <matejonnet@gmail.com>
2020-10-26 16:38:58 +0100

    Test that certain tasks are run also in case of cancel.

--- rest-service-workitem/src/test/java/org/jbpm/contrib/ActiveTasks.java
+++ rest-service-workitem/src/test/java/org/jbpm/contrib/ActiveTasks.java
@@ -1,8 +1,13 @@
 package org.jbpm.contrib;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.Semaphore;
 
 public class ActiveTasks {
+
+    private final Logger logger = LoggerFactory.getLogger(ActiveTasks.class);
 
     private Semaphore semaphore = new Semaphore(Integer.MAX_VALUE);
 
@@ -19,6 +24,8 @@ public class ActiveTasks {
     }
 
     public void waitAllCompleted() throws InterruptedException {
+        logger.debug("Waiting all completed...");
         semaphore.acquire(Integer.MAX_VALUE);
+        logger.debug("All completed.");
     }
 }
