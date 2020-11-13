31a2a8e25b6c7007bb6feb062de5a2f44fdb722a
Matej Lazar <matejonnet@gmail.com>
2020-11-13 20:20:10 +0100

    Retry failed http connections.

--- rest-service-workitem/src/test/java/org/jbpm/contrib/Zzzz.java
+++ rest-service-workitem/src/test/java/org/jbpm/contrib/Zzzz.java
@@ -5,23 +5,23 @@ import org.junit.Test;
 public class Zzzz {
     @Test
     public void X() {
-//if( !KieFunctions.isTrue(cancel) ) {
-//    Map resultToEvaluate = (Map)(KieFunctions.isTrue(noCallback) ? restResponse : callback);
-//    if( successEvalTemplate!=null ) {
-//        System.out.println("Evaluating service response:");
-//        System.out.println("Template:" + successEvalTemplate);
-//        System.out.println("Service response to evaluate:" + resultToEvaluate);
-//        if (TemplateRuntime.eval(successEvalTemplate, resultToEvaluate)) {
-//            kcontext.setVariable("status", "SUCCESS");
+//        if( !KieFunctions.isTrue(cancel) && KieFunctions.isEmpty(errorCause)) {
+//            Map resultToEvaluate = (Map)(KieFunctions.isTrue(noCallback) ? restResponse : callback);
+//            if( successEvalTemplate!=null ) {
+//                System.out.println("Evaluating service response:");
+//                System.out.println("Template:" + successEvalTemplate);
+//                System.out.println("Service response to evaluate:" + resultToEvaluate);
+//                if ((Boolean)TemplateRuntime.eval(successEvalTemplate, resultToEvaluate)) {
+//                    kcontext.setVariable("status", "SUCCESS");
+//                } else {
+//                    kcontext.setVariable("status", "FAILED");
+//                }
+//            } else {
+//                kcontext.setVariable("status","SUCCESS");
+//            }
 //        } else {
-//            kcontext.setVariable("status", "FAILED");
+//            kcontext.setVariable("status","CANCELLED");
 //        }
-//    } else {
-//        kcontext.setVariable("status","SUCCESS");
-//    }
-//} else {
-//    kcontext.setVariable("status","CANCELLED");
-//}
-}
+    }
 
 }
