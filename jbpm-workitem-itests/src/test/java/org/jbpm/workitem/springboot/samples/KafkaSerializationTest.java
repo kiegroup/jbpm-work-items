66599bafe5528e32359972af4f4e754a24b6acdd
Jason Porter <lightguard.jp@gmail.com>
2025-06-20 01:24:31 -0600

    Updating to latest tomcat v9 CVE related (#315)

--- jbpm-workitem-itests/src/test/java/org/jbpm/workitem/springboot/samples/KafkaSerializationTest.java
+++ jbpm-workitem-itests/src/test/java/org/jbpm/workitem/springboot/samples/KafkaSerializationTest.java
@@ -99,7 +99,7 @@ public class KafkaSerializationTest extends KafkaBaseTest {
         kafkaFixture.cleanup(deploymentService);
     }
 
-    @Test(timeout = 60000)
+    @Test(timeout = 120000)
     public void testKafkaWIHSendMessage() throws Exception {
         
         Long processInstanceId = processService.startProcess(deploymentId, 
